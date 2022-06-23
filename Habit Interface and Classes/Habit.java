11eab958b6266705c0e51083d931c9101e1d0d45
charlie <rcc116@txstate.edu>
2022-06-22 19:47:36 -0500

    fixed interface for the habit

--- Habit Interface and Classes/Habit.java
+++ Habit Interface and Classes/Habit.java
@@ -2,14 +2,19 @@
 
 public interface Habit 
 {
-    int numFailures = 0;
-
     
+
+      
     public void incrementFailures();
+    public void incrementSuccesses();
+    public int getNumFailures();
+    public int getNumSuccesses();
     public void setName(String habName);
+    public String getName();
     public void setNumFailures(int numFails);
     public void setReminders(int numReminders);
-    public void endHabit();
+    public int getReminders();
+    public String endHabit();
     
     
 }
