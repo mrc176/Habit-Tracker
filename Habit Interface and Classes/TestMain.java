b77bf2ec31e0808a01fb2bac52fa435e364bffb8
Charlie C <rcc116@txstate.edu>
2022-06-24 15:11:11 -0500

    test main

---
+++ Habit Interface and Classes/TestMain.java
@@ -0,0 +1,14 @@
+
+public class TestMain {
+    
+    public static void main(String args[])
+    {
+        Habit testHabit = new positiveHabit("Brush My Teeth", 2, 5);
+        
+        //HabitUI testUI = new HabitUI(testHabit);
+        
+        createHabitGUI createHabitGUI = new createHabitGUI(); 
+        
+    }
+    
+}
